ab81384eaf1f3ac5f5273034aca587c569c1c4af
Dindiaee <106719543+Dindiaee@users.noreply.github.com>
2022-06-15 22:23:23 +0700

    Update gas-oksigen (1).cpp

--- gas-oksigen (1).cpp
+++ gas-oksigen (1).cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
     int main (){
-    for(int i = 0; i > 5; i++){
-    for(int j = 0; j < 5; j++){
-    for(int k = 0; k < 5; k++){
+    for(int i = 0; i > 4; i++){
+    for(int j = 0; j < 4; j++){
+    for(int k = 0; k < 4; k++){
         cout << i << endl;
     }
         
